3cc9555b020acd71207d93077c58f8dfe0ea1588
TubbyStubby <tubbystubby2@gmail.com>
2022-04-03 21:05:45 +0530

    common store and tests

---
+++ src/classes/common_store/CommonStoreError.ts
@@ -0,0 +1,8 @@
+export class CommonStoreError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = this.constructor.name;
+        Object.setPrototypeOf(this, new.target.prototype);
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
---
+++ src/classes/common_store/index.ts
@@ -0,0 +1,2 @@
+export * from './CommonStoreError';
+export * from './CommonStore';
