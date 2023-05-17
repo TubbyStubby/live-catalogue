d2519a58e9443548147ddda378a83e8c2f6717d6
TubbyStubby <tubbystubby2@gmail.com>
2023-05-18 00:10:13 +0530

    removed unnecessary stuff from error class

--- src/classes/live_catalogue/LiveCatalogueError.ts
+++ src/classes/live_catalogue/LiveCatalogueError.ts
@@ -2,7 +2,5 @@ export class LiveCatalogueError extends Error {
     constructor(message?: string) {
         super(message);
         this.name = this.constructor.name;
-        Object.setPrototypeOf(this, new.target.prototype);
-        Error.captureStackTrace(this, this.constructor);
     }
 }
--- src/classes/pub_sub_handler/PubSubHandlerError.ts
+++ src/classes/pub_sub_handler/PubSubHandlerError.ts
@@ -2,7 +2,5 @@ export class PubSubHandlerError extends Error {
     constructor(message?: string) {
         super(message);
         this.name = this.constructor.name;
-        Object.setPrototypeOf(this, new.target.prototype);
-        Error.captureStackTrace(this, this.constructor);
     }
 }
